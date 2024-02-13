ae4e098ebf8963a0c5fd5d2b675526504255a037
Angel Yvan Choquehuanca Peraltilla <achoquehuancaper@gmail.com>
2024-02-13 13:11:01 -0500

    Agregando todos los dedos

--- sisrevimof-arduino.ino
+++ sisrevimof-arduino.ino
@@ -13,19 +13,25 @@
 MPU6050 mpu(Wire);
 long timer = 0;
 
-const int FLEX_THUMB = 12; // Pin connected to voltage divider output
+const int FLEX_THUMB = 1; // Pin connected to voltage divider output
 const int FLEX_INDEX = 2; // Pin connected to voltage divider output
-// Measure the voltage at 5V and the actual resistance of your
-// 47k resistor, and enter them below:
-const float VCC = 3.3; // Measured voltage of Ardunio 5V line
-const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+const int FLEX_MIDDLE = 3; // Pin connected to voltage divider output
+const int FLEX_RING = 6; // Pin connected to voltage divider output
+const int FLEX_PINKY = 7; // Pin connected to voltage divider output
 
-// Upload the code, then try to adjust these values to more
-// accurately calculate bend degree.
-//const float STRAIGHT_RESISTANCE = 37300.0; // resistance when straight
-//const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
-const float STRAIGHT_RESISTANCE = 270000.0; // resistance when straight
-const float BEND_RESISTANCE = 430000.0; // resistance at 90 deg
+//Measurements for flex sensors
+//412 - 322 - 326 - 462 - 470 -> 0
+//220 - 190 - 170 - 360 - 370 -> 90
+const int MAX_THUMB = 412;
+const int MIN_THUMB = 220;
+const int MAX_INDEX = 322;
+const int MIN_INDEX = 190;
+const int MAX_MIDDLE = 326;
+const int MIN_MIDDLE = 170;
+const int MAX_RING = 462;
+const int MIN_RING = 360;
+const int MAX_PINKY = 470;
+const int MIN_PINKY = 370;
 
 void setup() {
   Serial.begin(115200);
@@ -54,52 +60,34 @@ void loop() {
   String magAzi = "";
   if(millis() - timer > 100){ // print data every second
     int flexADCThumb = analogRead(FLEX_THUMB);
-    //float flexVThumb = flexADCThumb * VCC / 1023.0;
-    float flexVThumb = flexADCThumb * VCC / 4095.0;
-    float flexRThumb = R_DIV * (VCC / flexVThumb - 1.0);
-    //Serial.println("Resistance: " + String(flexRThumb) + " ohms");
-  
-    // Use the calculated resistance to estimate the sensor's
-    // bend angleThumb:
-    float angleThumb = map(flexRThumb, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
-                      0, 90.0);
-  
-    // Read the ADC, and calculate voltage and resistance from it
-    int flexADCIndex = analogRead(FLEX_INDEX);
-    //float flexVIndex = flexADCIndex * VCC / 1023.0;
-    float flexVIndex = flexADCIndex * VCC / 4095.0;
-    float flexRIndex = R_DIV * (VCC / flexVIndex - 1.0);
-    //Serial.println("Resistance: " + String(flexRIndex) + " ohms");
-  
-    // Use the calculated resistance to estimate the sensor's
-    // bend angleIndex:
-    float angleIndex = map(flexRIndex, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
-                      0, 90.0);
+    int flexADCIndex= analogRead(FLEX_INDEX);
+    int flexADCMiddle = analogRead(FLEX_MIDDLE);
+    int flexADCRing = analogRead(FLEX_RING);
+    int flexADCPinky = analogRead(FLEX_PINKY);
+
+    float angleThumb = map(flexADCThumb, MIN_THUMB, MAX_THUMB, 0, 90.0);
+    float angleIndex = map(flexADCIndex, MIN_INDEX, MAX_INDEX, 0, 90.0);
+    float angleMiddle = map(flexADCMiddle, MIN_MIDDLE, MAX_MIDDLE, 0, 90.0);
+    float angleRing = map(flexADCRing, MIN_RING, MAX_RING, 0, 90.0);
+    float anglePinky = map(flexADCPinky, MIN_PINKY, MAX_PINKY, 0, 90.0);
+
     data = "RightThumb=";
     data += String(angleThumb);
     data += "&RightIndex=";
     data += String(angleIndex);
-    data += "&RightMiddle=0.00";
-    //data += analogRead(A1);
-    data += "&RightRing=0.00";
-    //data += analogRead(A1);
-    data += "&RightPinky=0.00";
-    //data += analogRead(A1);
+    data += "&RightMiddle=";
+    data += String(angleMiddle);
+    data += "&RightRing=";
+    data += String(angleRing);
+    data += "&RightPinky=";
+    data += String(anglePinky);
     data += "&RightAngX=";
     data += String(mpu.getAngleX());
     data += "&RightAngY=";
     data += String(mpu.getAngleY());
     data += "&RightAngZ=";
     data += String(mpu.getAngleZ());
-    /*data += "&RightMagX=";
-    data += String(magX);
-    data += "&RightMagY=";
-    data += String(magY);
-    data += "&RightMagZ=";
-    data += String(magZ);
-    data += "&RightAziZ=";
-    data += String(magAzi);
-    Serial.println(data);*/
+  
     data += "&RightAccX=";
     data += String(mpu.getAccX());
     data += "&RightAccY=";
@@ -113,23 +101,6 @@ void loop() {
     data += "&RightGyroZ=";
     data += String(mpu.getGyroZ());
     Serial.println(data);
-    
-    /*Serial.print(F("TEMPERATURE: "));Serial.printºln(mpu.getTemp());
-    Serial.print(F("ACCELERO  X: "));Serial.print(mpu.getAccX());
-    Serial.print("\tY: ");Serial.print(mpu.getAccY());
-    Serial.print("\tZ: ");Serial.println(mpu.getAccZ());
-  
-    Serial.print(F("GYRO      X: "));Serial.print(mpu.getGyroX());
-    Serial.print("\tY: ");Serial.print(mpu.getGyroY());
-    Serial.print("\tZ: ");Serial.println(mpu.getGyroZ());
-  
-    Serial.print(F("ACC ANGLE X: "));Serial.print(mpu.getAccAngleX());
-    Serial.print("\tY: ");Serial.println(mpu.getAccAngleY());
-    
-    Serial.print(F("ANGLE     X: "));Serial.print(mpu.getAngleX());
-    Serial.print("\tY: ");Serial.print(mpu.getAngleY());
-    Serial.print("\tZ: ");Serial.println(mpu.getAngleZ());
-    Serial.println(F("=====================================================\n"));*/
     
     timer = millis();
   }
